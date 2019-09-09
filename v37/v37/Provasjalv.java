f80bdad1cc10e90c696d4b968e251cb09d68b462
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-09-09 13:03:03 +0200

    Genomgång lektion 1 v37

---
+++ v37/v37/Provasjalv.java
@@ -0,0 +1,13 @@
+package v37;
+
+public class Provasjalv {
+	
+	public static void main(String[] args) {
+		
+		int radie = 2; 
+		double pi = 3.14;
+		
+		System.out.println("Arean av en cirkel med radien 2cm är " + radie*radie*pi + "cm^2");
+	}
+
+}
---
+++ v37/v37/lektion1.java
@@ -0,0 +1,43 @@
+package v37;
+
+public class lektion1 {
+
+	public static void main(String[] args) {
+		// Ett sätt att skriva kommentarer
+		/*Gör så här om du vill 
+		 * skriva kommentarer 
+		 * på flera rader
+		 */
+
+		
+		
+		int heltal1 = 5;  // Skapar en låda som heter heltal1 och fyller den med ett heltal
+		int svar; // Deklarera 
+		
+		svar = heltal1*heltal1; 
+		
+		System.out.println("Svar: " + heltal1*heltal1); // skriv syso sedan tryck ctrl+space för att slippa skriva hela kommandot (System.out.println();  )
+														// + efter "Svar: " kallas konkatinera 
+		
+		
+		double pi =  3.14;
+		int radie = 5;
+		
+		double area = pi*radie*radie;
+		
+		System.out.println("Arean är: " + area + " cm^2");
+		
+		
+		
+		long stortheltal = 5000000000000000000L;
+		
+		System.out.println(stortheltal +1);
+		
+		//int (heltal), double (decimaltal), long (stora heltal), 
+		
+		
+		
+		
+	}
+
+}
