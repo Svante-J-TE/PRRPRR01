e81638b78c330e6139be2646b5e554e420e479ef
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-09-10 12:11:53 +0200

    Skapa String och variabel (input)

--- v37/v37/laxaV37.java
+++ v37/v37/laxaV37.java
@@ -1,10 +1,15 @@
 package v37;
 
+import java.util.Scanner;
+
 public class laxaV37 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		
 	}
 
 }
