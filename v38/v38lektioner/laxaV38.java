14b62939b100b2cac5b0d1a074708e8ef0aa4d1b
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-09-17 11:50:30 +0200

    Skapat scanner och fem variabler

---
+++ v38/v38lektioner/laxaV38.java
@@ -0,0 +1,31 @@
+package v38lektioner;
+
+import java.util.Scanner;
+
+public class laxaV38 {
+
+	public static void main(String[] args) {
+
+		int number1 = 0;
+		int number2 = 0;
+		int number3 = 0;
+		int number4 = 0;
+		int number5 = 0;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Skriv fem stycken heltal: ");
+
+		number1 = input.nextInt();
+
+		number2 = input.nextInt();
+
+		number3 = input.nextInt();
+
+		number4 = input.nextInt();
+
+		number5 = input.nextInt();
+		
+	}
+
+}
