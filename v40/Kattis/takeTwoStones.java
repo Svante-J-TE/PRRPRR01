9d50889e82a81f5e2f3766a3b283027a6bade545
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-09-30 13:09:32 +0200

    färdig och inlämnad på Kattis

---
+++ v40/Kattis/takeTwoStones.java
@@ -0,0 +1,24 @@
+package Kattis;
+
+import java.util.Scanner;
+
+public class takeTwoStones {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int N = input.nextInt();
+	
+			if(N % 2 != 0) {
+				System.out.println("Alice");
+			}
+			
+			else if(N % 2 == 0) {
+				System.out.println("Bob");
+			}
+		
+
+	}
+
+}
