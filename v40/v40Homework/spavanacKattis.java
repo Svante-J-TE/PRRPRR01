37adc73028b9862b4a0b9ea161f611e0cd05a9b7
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-10-03 09:18:35 +0200

    created scanner

---
+++ v40/v40Homework/spavanacKattis.java
@@ -0,0 +1,19 @@
+package v40Homework;
+
+import java.util.Scanner;
+
+public class spavanacKattis {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int h = input.nextInt();
+		int m = input.nextInt();
+		
+		System.out.println(h +" "+ m);
+		
+		
+	}
+
+}
