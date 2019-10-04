cc0882dc87989e9ca1142dd895bf06a5ed80f329
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-10-04 09:38:35 +0200

    lade till kommentarer och bytte namn på variabler

--- v40/v40Homework/spavanacKattis.java
+++ v40/v40Homework/spavanacKattis.java
@@ -8,20 +8,20 @@ public class spavanacKattis {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int Hours = input.nextInt();
-		int minutes = input.nextInt();
+		int Timmar = input.nextInt();
+		int Minuter = input.nextInt();
 		
-		minutes -= 45;
+		Minuter -= 45; // Tar bort 45 från variabeln
 
-		if (minutes < 0)
+		if (Minuter < 0) // minuter blir mindre än 0 efter detta gör den denna if sats 
 		    {
-		    Hours -= 1;
-		    minutes += 60;
+		    Timmar -= 1;
+		    Minuter += 60;
 		    }
-		if (Hours < 0)
-		    Hours = 23;
+		if (Timmar < 0) // om timmarna då är mindre än 0 så läggs det till 23 för att innan 12 på natten kommer 11 (23)
+		    Timmar = 23;
 		
-		System.out.println(Hours +" "+ minutes);
+		System.out.println(Timmar +":"+ Minuter);
 		
 		
 	}
