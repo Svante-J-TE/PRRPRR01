a78d0183d97dc876628fa642b666aa7fd2a5e4b8
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2019-11-04 12:09:56 +0100

    changed name to fit task

--- v43/homework/homework2.java
+++ v43/homework/homework2.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class homework2 {
 
 	public static void main(String[] args) {
-		System.out.println("Det största talet är: " + min());
+		System.out.println("Det största talet är: " + max());
 	}
 /**
  * Tar två heltal och skriver ut det största
  * @return
  */
-	public static int min() {
+	public static int max() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Skriv in två heltal:");
 		int a = input.nextInt();
